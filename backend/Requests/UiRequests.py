5e71940509338cb821d285d2e630699ea43701e3
gapls <125091918+G4PLS@users.noreply.github.com>
2024-07-17 22:53:50 +0200

    Added Ui GET Requests

---
+++ backend/Requests/UiRequests.py
@@ -0,0 +1,23 @@
+import uuid
+
+from .OBSRequests import OBSRequest, request_error_handler
+from obswebsocket import obsws, requests
+
+from GetRequestContent.UiContent import *
+from GetRequestContent.GetRequestContent import convert_single
+
+
+class UiRequest(OBSRequest):
+    @staticmethod
+    @request_error_handler
+    def get_studio_mode_enabled(obs: obsws) -> bool:
+        """GetStudioModeEnabled"""
+        reqeust_body = obs.call(requests.GetStudioModeEnabled())
+        return convert_single(reqeust_body, "studioModeEnabled")
+
+    @staticmethod
+    @request_error_handler
+    def get_monitor_list(obs: obsws) -> MonitorList:
+        """GetMonitorList"""
+        reqeust_body = obs.call(requests.GetMonitorList())
+        return MonitorList.from_request_body(reqeust_body)
