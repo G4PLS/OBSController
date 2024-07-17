2c0864bd27979185ccc505999bc6cd2ead08f929
gapls <125091918+G4PLS@users.noreply.github.com>
2024-07-17 22:54:18 +0200

    Updated Transition Requests and added TransitionContent.py

--- backend/Requests/TransitionRequests.py
+++ backend/Requests/TransitionRequests.py
@@ -1,36 +1,35 @@
 from .OBSRequests import OBSRequest, request_error_handler
 from obswebsocket import obsws, requests
 
-from RequestFormatters.TransitionFormatters import *
+from GetRequestContent.TransitionContent import *
+from GetRequestContent.GetRequestContent import convert_single
 
 
 class TransitionKindRequest(OBSRequest):
     @staticmethod
     @request_error_handler
-    def get(obs: obsws) -> TransitionKindFormatter:
+    def get_transition_kinds(obs: obsws) -> list[str]:
+        """GetTransitionKindList"""
         request_body = obs.call(requests.GetTransitionKindList())
-        return TransitionKindFormatter.from_request_body(request_body)
+        return convert_single(request_body, "transitionKinds")
 
-
-class SceneTransitionRequest(OBSRequest):
     @staticmethod
     @request_error_handler
-    def get(obs: obsws) -> SceneTransitionFormatter:
+    def get_transitions(obs: obsws) -> SceneTransitionList:
+        """GetSceneTransitionList"""
         request_body = obs.call(requests.GetSceneTransitionList())
-        return SceneTransitionFormatter.from_request_body(request_body)
+        return SceneTransitionList.from_request_body(request_body)
 
-
-class CurrentSceneTransitionRequest(OBSRequest):
     @staticmethod
     @request_error_handler
-    def get(obs: obsws) -> CurrentSceneTransitionFormatter:
+    def get_current_transition(obs: obsws) -> CurrentTransition:
+        """GetCurrentSceneTransition"""
         request_body = obs.call(requests.GetCurrentSceneTransition())
-        return CurrentSceneTransitionFormatter.from_request_body(request_body)
+        return CurrentTransition.from_request_body(request_body)
 
-
-class TransitionCursorRequest(OBSRequest):
     @staticmethod
     @request_error_handler
-    def get(obs: obsws) -> SceneTransitionCursorFormatter:
+    def get_current_transition_cursor(obs: obsws) -> int:
+        """GetCurrentSceneTransitionCursor"""
         request_body = obs.call(requests.GetCurrentSceneTransitionCursor())
-        return SceneTransitionCursorFormatter.from_request_body(request_body)
+        return convert_single(request_body, "transitionCursor")
