b4bf46da3143a6582017ac47bfce971493f0f717
gapls <125091918+G4PLS@users.noreply.github.com>
2024-07-21 18:52:56 +0200

    Added SET Requests for StreamRequests.py

--- backend/Requests/StreamRequests.py
+++ backend/Requests/StreamRequests.py
@@ -11,3 +11,27 @@ class StreamRequests(OBSRequest):
         """GetStreamStatus"""
         request_body = obs.call(requests.GetStreamStatus())
         return StreamStatus.from_request_body(request_body)
+
+    @staticmethod
+    @request_error_handler
+    def toggle_stream(obs: obsws):
+        """ToggleStream"""
+        obs.call(requests.ToggleStream())
+
+    @staticmethod
+    @request_error_handler
+    def start_stream(obs: obsws):
+        """StartStream"""
+        obs.call(requests.StartStream())
+
+    @staticmethod
+    @request_error_handler
+    def stop_stream(obs: obsws):
+        """StopStream"""
+        obs.call(requests.StopStream())
+
+    @staticmethod
+    @request_error_handler
+    def send_stream_caption(obs: obsws, caption: str):
+        """SendStreamCaption"""
+        obs.call(requests.SendStreamCaption(captionText=caption))
