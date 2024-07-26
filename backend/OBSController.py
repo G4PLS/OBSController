8b7580d55e9e4db97eda5a0642de76773212ad98
gapls <125091918+G4PLS@users.noreply.github.com>
2024-07-26 19:11:03 +0200

    Added host ip check to Controller

--- backend/OBSController.py
+++ backend/OBSController.py
@@ -1,19 +1,21 @@
+import ipaddress
+
 import obswebsocket.exceptions
 from obswebsocket import obsws
 
 from loguru import logger as log
 import websocket
+from Requests.GeneralRequests import GeneralRequest
 
 class OBSController(obsws):
     def __init__(self):
         self.connected: bool = False
         self.event_obs: obsws = None
-        self.version: VersionFormatter = None
 
     def on_connect(self, obs: obsws):
         self.connected = True
 
-    def on_disconnect(self, obs):
+    def on_disconnect(self, obs: obsws):
         self.connected = False
 
     def register(self, *args, **kwargs):
@@ -26,15 +28,41 @@ class OBSController(obsws):
         except (obswebsocket.exceptions.MessageTimeout,  websocket._exceptions.WebSocketConnectionClosedException, KeyError) as e:
             log.error(e)
 
+    def unregister(self, *args, **kwargs):
+        try:
+            self.event_obs.unregister(*args, **kwargs)
+        except (obswebsocket.exceptions.MessageTimeout,  websocket._exceptions.WebSocketConnectionClosedException, KeyError) as e:
+            log.error(e)
+
+    def validate_host(self, host: str):
+        if host == 'localhost':
+            return True
+        try:
+            ipaddress.ip_address(host)
+            return True
+        except ValueError as e:
+            log.error(e)
+
+            log.info("Disconnecting OBSController and Event Listener")
+            if self.connected:
+                self.disconnect()
+            if (self.event_obs is not None and
+                    self.event_obs.ws is not None):
+                self.event_obs.disconnect()
+
+            return False
+
     def _connect(self, **kwargs):
         super().__init__(on_connect=self.on_connect, on_disconnect=self.on_disconnect, **kwargs)
         self.event_obs = obsws(on_connect=self.on_connect, on_disconnect=self.on_disconnect, **kwargs)
         self.connect()
-        #self.get_version()
-        #log.info(f"Successfully connected to OBS {self.version.OBS_VERSION} "
-        #         f"under {kwargs.get("host")}:{kwargs.get("port")}")
+        version = GeneralRequest.get_version(self)
+        log.info(f"Successfully connected to OBS {version.OBS_VERSION} under {kwargs.get("host"):{kwargs.get("port")}}")
 
-    def open_connection(self, host: str = 'localhost', port: int = 4455, password: str = "", legacy: bool = False, **kwargs) -> bool:
+    def connect_to_obs(self, host: str = 'localhost', port: int = 4455, password: str = "", legacy: bool = False, **kwargs):
+        if not self.validate_host(host):
+            return
+
         try:
             self._connect(host=host, port=port, password=password, legacy=legacy, **kwargs)
             return True
@@ -46,7 +74,3 @@ class OBSController(obsws):
             except (obswebsocket.exceptions.ConnectionFailure, ValueError) as e:
                 log.error(f"Failed to connect to OBS: {e}")
         return False
-
-    #
-    # GENERAL REQUESTS
-    #
