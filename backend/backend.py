d93761edb8c0c1c9e4d59d85d687cbd13b9afe89
gapls <125091918+G4PLS@users.noreply.github.com>
2024-07-28 00:11:12 +0200

    Added first Actions

--- backend/backend.py
+++ backend/backend.py
@@ -1,17 +1,18 @@
 from streamcontroller_plugin_tools import BackendBase
 from OBSController import OBSController
 from Requests import *
+from loguru import logger as log
 
 class Backend(BackendBase):
     def __init__(self):
-        #super().__init__()
+        super().__init__()
+        log.warning("STARTING BACKEND")
         self.obs_controller = OBSController()
         self.obs_controller.connect_to_obs(
             host=self.get_setting("ip", "localhost"),
             port=self.get_setting("port", 4455),
             password=self.get_setting("password", "")
         )
-        #self.obs_controller.connect_to_obs()
 
     def get_setting(self, key: str, default=None):
         return self.frontend.get_settings().get(key, default)
@@ -22,6 +23,8 @@ class Backend(BackendBase):
     #
     # RECORD
     #
+    def get_record_status(self):
+        return RecordRequests.get_record_status(self.obs_controller)
 
     def start_record(self):
         RecordRequests.start_record(self.obs_controller)
@@ -31,3 +34,33 @@ class Backend(BackendBase):
 
     def pause_record(self):
         RecordRequests.pause_record(self.obs_controller)
+
+    def resume_record(self):
+        RecordRequests.resume_record(self.obs_controller)
+
+    def toggle_record(self):
+        return RecordRequests.toggle_record(self.obs_controller)
+
+    def toggle_pause(self):
+        RecordRequests.toggle_pause(self.obs_controller)
+
+    def split_record_file(self):
+        RecordRequests.split_record_file(self.obs_controller)
+
+    #
+    # STREAM
+    #
+
+    def get_stream_status(self):
+        return StreamRequests.get_stream_status(self.obs_controller)
+
+    def start_stream(self):
+        StreamRequests.start_stream(self.obs_controller)
+
+    def stop_stream(self):
+        StreamRequests.stop_stream(self.obs_controller)
+
+    def toggle_stream(self):
+        StreamRequests.toggle_stream(self.obs_controller)
+
+backend = Backend()
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Import StreamController modules
 import os
 
-import pulsectl
+from .actions.RecordAction.Record import RecordAction
+
 from src.backend.PluginManager.ActionHolder import ActionHolder
 from src.backend.PluginManager.ActionInputSupport import ActionInputSupport
 from src.backend.PluginManager.PluginBase import PluginBase
@@ -14,6 +15,23 @@ class OBSController(PluginBase):
         self.init_vars()
 
         self.launch_backend(os.path.join(self.PATH, "backend", "backend.py"), os.path.join(self.PATH, "backend", ".venv"), open_in_terminal=False)
+
+        #
+        # ACTION HOLDERS
+        #
+
+        self.record_action_holder = ActionHolder(
+            plugin_base=self,
+            action_base=RecordAction,
+            action_id_suffix="Record",
+            action_name="Recording",
+            action_support= {
+                Input.Key: ActionInputSupport.SUPPORTED,
+                Input.Dial: ActionInputSupport.UNSUPPORTED,
+                Input.Touchscreen: ActionInputSupport.UNSUPPORTED
+            }
+        )
+        self.add_action_holder(self.record_action_holder)
 
         self.register()
 
