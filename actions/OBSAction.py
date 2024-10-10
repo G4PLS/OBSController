95282435b564dbc7f2c0a6124e864e868e45c3ff
gapls <125091918+G4PLS@users.noreply.github.com>
2024-10-10 14:38:37 +0200

    Feat(OBSAction): Added automatic startup of the Plugin Config Screen when adding the first action

--- actions/OBSAction.py
+++ actions/OBSAction.py
@@ -17,11 +17,27 @@ class OBSAction(ActionBase):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.plugin_config = PluginConfigButton(self.plugin_base, OBSConfigWindow, True)
+        self.has_configuration = True
+        self.obs_config_setup = False
+
+    def on_ready(self):
+        plugin_settings = self.plugin_base.get_settings()
+        self.obs_config_setup = plugin_settings.get("first-setup", False)
 
     def get_custom_config_area(self):
         self.ui = Adw.PreferencesGroup()
 
         self.plugin_config.unparent()
         self.ui.add(self.plugin_config)
+
+        if not self.obs_config_setup:
+            self.plugin_config.open_config_window()
+            self.obs_config_setup = True
+
+            plugin_settings = self.plugin_base.get_settings()
+            plugin_settings["first-setup"] = self.obs_config_setup
+            self.plugin_base.set_settings(plugin_settings)
+
+            print(self.obs_config_setup)
 
         return self.ui
