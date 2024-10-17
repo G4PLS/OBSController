e764bbe2814e76180f43cf2326446f1965f15361
gapls <125091918+G4PLS@users.noreply.github.com>
2024-10-18 00:10:09 +0200

    Feat(ReconnectAction): Re-Implemented ReconnectAction.py to use the new systems

--- actions/OBSActions/ReconnectAction.py
+++ actions/OBSActions/ReconnectAction.py
@@ -1,35 +1,17 @@
-from src.backend.DeckManagement.Media.ImageLayer import ImageLayer
-from src.backend.DeckManagement.Media.Media import Media
-
 from ..OBSAction import OBSAction
+
 
 class ReconnectAction(OBSAction):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.has_configuration = False
+
         self.plugin_base.connect_to_backend_event("com.gapls.OBSController::OBSEvent", "on_exit_started", self.obs_exit_event)
 
-        self.established = Media(
-            size=0.95,
-            layers=[
-                ImageLayer.from_media_path(self.get_asset_path("obs.svg", "OBS")),
-                ImageLayer.from_media_path(self.get_asset_path("connection_established.svg", "OBS"))
-            ]
-        ).get_final_media()
-        self.lost = Media(
-            size=0.95,
-            layers=[
-                ImageLayer.from_media_path(self.get_asset_path("obs.svg", "OBS")),
-                ImageLayer.from_media_path(self.get_asset_path("connection_lost.svg", "OBS"))
-            ]
-        ).get_final_media()
+    #
+    # ACTION EVENTS
+    #
 
-    def obs_exit_event(self, *args):
-        self.update_status(self.plugin_base.backend.get_connected())
-
-    def on_ready(self):
-        super().on_ready()
-
+    def on_update(self):
         self.update_status(self.plugin_base.backend.get_connected())
 
     def on_key_down(self):
@@ -37,6 +19,13 @@ class ReconnectAction(OBSAction):
 
     def update_status(self, connection_status: bool):
         if connection_status:
-            self.set_media(image=self.established)
+            self.set_media(self.plugin_base.asset_manager.CONNECTED_MEDIA)
         else:
-            self.set_media(image=self.lost)
+            self.set_media(self.plugin_base.asset_manager.DISCONNECTED_MEDIA)
+
+    #
+    # EVENTS
+    #
+
+    async def obs_exit_event(self, *args):
+        self.update_status(self.plugin_base.backend.get_connected())
