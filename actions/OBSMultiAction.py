a9dd7395c61d7527391be222fab58375a03ab3df
gapls <125091918+G4PLS@users.noreply.github.com>
2024-10-18 00:06:58 +0200

    Feat(Actions): Added Multi and Normal OBSAction for more specialized Ui

--- actions/OBSMultiAction.py
+++ actions/OBSMultiAction.py
@@ -1,14 +1,11 @@
 from gi.repository import Adw
 
-from src.backend.DeckManagement.Media.ImageLayer import ImageLayer
-from src.backend.DeckManagement.Media.Media import Media
-from src.backend.PluginManager.ActionBase import ActionBase
 from ..internal.MultiAction.MultiAction import MultiAction
 from ..internal.PluginConfig import PluginConfigButton
 from ..internal.OBSConfig import OBSConfigWindow
 
 
-class OBSAction(ActionBase):
+class OBSMultiAction(MultiAction):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -16,15 +13,17 @@ class OBSAction(ActionBase):
         self.has_configuration = True
         self.obs_config_setup = False
 
-    def on_ready(self):
+    def load_settings(self):
+        super().load_settings()
+
         plugin_settings = self.plugin_base.get_settings()
         self.obs_config_setup = plugin_settings.get("first-setup", False)
 
-    def get_custom_config_area(self):
-        self.ui = Adw.PreferencesGroup()
+    def build_ui(self, ui: Adw.PreferencesGroup = None) -> Adw.PreferencesGroup:
+        ui = Adw.PreferencesGroup()
 
         self.plugin_config.unparent()
-        self.ui.add(self.plugin_config)
+        ui.add(self.plugin_config)
 
         if not self.obs_config_setup:
             self.plugin_config.open_config_window()
@@ -34,4 +33,19 @@ class OBSAction(ActionBase):
             plugin_settings["first-setup"] = self.obs_config_setup
             self.plugin_base.set_settings(plugin_settings)
 
-        return self.ui
+        return super().build_ui(ui)
+
+    def on_action_changed(self, *args):
+        settings = {"action-lookup": self.action_translation}
+        self.set_settings(settings)
+
+        self.set_top_label("")
+        self.set_center_label("")
+        self.set_bottom_label("")
+
+        self.set_background_color(color=[0,0,0,0])
+        self.set_media()
+
+        super().on_action_changed()
+
+        self.on_update()
